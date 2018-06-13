a3c072d4dd662b939dd0828b7c12b8b9a940b733
TalhaAwan <talhamanzoorawan@gmail.com>
2018-06-14 01:55:56 +0500

    return main index file from dist instead of client

--- server/routes.js
+++ server/routes.js
@@ -15,6 +15,6 @@ module.exports = function (app) {
     // All other routes should redirect to the index.html
     app.route('/*')
         .get(function (req, res) {
-            res.sendFile(path.resolve(app.get('appPath') + '/index.html'));
+            res.sendFile(path.resolve(app.get('distPath') + '/index.html'));
         })
 }
